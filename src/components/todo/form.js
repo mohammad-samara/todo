50f65367ac0a185f2636ccfbc90e7eaaccf5e5aa
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-18 15:57:18 +0300

    convert from class-based to functions

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,49 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-class TodoForm extends React.Component {
+function TodoForm(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = { item: {} };
-  }
-  handleInputChange = e => {
-    this.setState({ item: {...this.state.item, [e.target.name]: e.target.value } });
+  const [item, setItem] = useState({});
+  const handleInputChange = e => {
+    setItem({...item, [e.target.name]: e.target.value });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
-    this.props.handleSubmit(this.state.item);
-    const item = {};
-    this.setState({item});
+    props.handleSubmit(item);
+    this.setState({});
   };
 
-  render() {
+
     return (
       <>
         <h3>Add Item</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label>
             <span>To Do Item</span>
             <input
               name="text"
               placeholder="Add To Do List Item"
-              onChange={this.handleInputChange}
+              onChange={handleInputChange}
             />
           </label>
           <label>
             <span>Difficulty Rating</span>
-            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={this.handleInputChange} />
+            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
           </label>
           <label>
             <span>Assigned To</span>
-            <input type="text" name="assignee" placeholder="Assigned To" onChange={this.handleInputChange} />
+            <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
           </label>
           <button>Add Item</button>
         </form>
       </>
     );
-  }
+
 }
 
 export default TodoForm;
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-class TodoList extends React.Component {
+function TodoList(props) {
 
-  render() {
+
     return (
       <ul>
-        {this.props.list.map(item => (
+        {props.list.map(item => (
           <li
             className={`complete-${item.complete.toString()}`}
             key={item._id}
           >
-            <span onClick={() => this.props.handleComplete(item._id)}>
+            <span onClick={() => props.handleComplete(item._id)}>
               {item.text}
             </span>
           </li>
         ))}
       </ul>
     );
-  }
+
 }
 
 export default TodoList;
