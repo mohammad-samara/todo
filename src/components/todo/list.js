e48de1b7ce4c5c16997d193ac991ddb69fc71a95
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-19 15:52:17 +0300

    first run

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function TodoList(props) {
@@ -7,15 +9,17 @@ function TodoList(props) {
     return (
       <ListGroup>
       {props.list.map((item) => (
-        <ListGroup.Item
-          variant={item.complete ? 'success' : 'danger'}
-          // className={`complete-${item.complete.toString()}`}
+        <Card
+          // variant={item.complete ? 'success' : 'danger'}
+          className={`complete-${item.complete.toString()}`}
           key={item._id}
         >
-          <span onClick={() => props.handleComplete(item._id)}>
-            {item.text}
-          </span>
-        </ListGroup.Item>
+          <Card.Header>{item.complete}  {item.assignee}</Card.Header>
+          <Card.Text onClick={() => props.handleComplete(item._id)}>
+            {item.text}  {item.difficulty }
+          </Card.Text>
+          <Button variant="danger" className='delete' onClick={() => props.handleDelete(item._id)}>X</Button>
+        </Card>
       ))}
     </ListGroup>
     );
